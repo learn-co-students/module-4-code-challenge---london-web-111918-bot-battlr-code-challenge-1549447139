cd071b1b30e11f6f8b0d4af4b15611ba7bc78ec3
github username <github email address>
2019-02-06 10:37:46 +0000

    reached checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,41 @@
 import React from "react";
+import BotsCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
+const API = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state = {
+    allBots: [],
+    botArmy: []
+  }
+
+  componentDidMount() {
+    return fetch(API)
+    .then(res => res.json())
+    .then(data => this.setState({ allBots: data }))
+  }
+
+  handleClick = (bot) => {
+    let newArray
+    const army = this.state.botArmy
+    if(army.includes(bot)) {
+    army.splice(army.indexOf(bot), 1)
+    newArray = army
+    } else {
+    newArray = [...army, bot]
+    }
+    this.setState({
+      botArmy: newArray
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy allBots={this.state.botArmy} handleClick={this.handleClick} />
+        <BotsCollection allBots={this.state.allBots} handleClick={this.handleClick} />
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -9,8 +9,8 @@ class YourBotArmy extends React.Component {
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            <h1>Your Bot Army</h1>
+            {this.props.allBots.map(bot => <BotCard key={bot.id} bot={bot} handleClick={this.props.handleClick} />)}
           </div>
         </div>
       </div>
